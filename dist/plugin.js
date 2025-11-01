230a75d95342f942eb36932bfc1117a354bbc10b
Noman <130601057+8gudbits@users.noreply.github.com>
2025-11-01 11:27:10 +0600

    Update plugin.js

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,10 +1,7 @@
 exports.repo = "8gudbits/hfs-chat"
 exports.version = 1.0
 exports.description = "damienzonly/hfs-chat v1.4 with UI improvements"
-exports.preview = [
-  "https://github.com/8gudbits/hfs-chat-plus/raw/main/preview.png",
-  "https://github.com/8gudbits/hfs-chat-plus/raw/main/config.png",
-]
+exports.preview = "https://github.com/8gudbits/hfs-chat-plus/raw/main/preview.png"
 exports.apiRequired = 8.87
 exports.frontend_js = "main.js"
 exports.frontend_css = "style.css"
